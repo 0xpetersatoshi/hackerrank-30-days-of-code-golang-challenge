834a29ca24e860401222e1c1759077ba1fa18386
pbegle <peterbegle@gmail.com>
2019-02-12 14:27:45 -0800

    Updated solution to 001 to include accepted solution on HackerRank.

--- 001-data-types/main.go
+++ 001-data-types/main.go
@@ -3,28 +3,37 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"os"
 	"strconv"
-	"strings"
 )
 
 func main() {
 	var _ = strconv.Itoa // Ignore this comment. You can still use the package "strconv".
 
-	// var i uint64 = 4
-	// var d float64 = 4.0
-	// var s string = "HackerRank "
+	var i uint64 = 4
+	var d float64 = 4.0
+	var s string = "HackerRank "
 
-	logstring :=
-		`12
-4.0
-is the best place to learn and practice coding!`
+	scanner := bufio.NewScanner(os.Stdin)
 
-	scanner := bufio.NewScanner(strings.NewReader(logstring))
-	scanner.Split(bufio.ScanLines)
+	var incomingInt int64
+	var incomingFloat float64
+	var incomingString string
+	inputVals := []string{}
 
 	for scanner.Scan() {
 		input := scanner.Text()
-
-		fmt.Println(input)
+		if len(input) == 0 {
+			break
+		}
+		inputVals = append(inputVals, input)
 	}
+
+	incomingInt, _ = strconv.ParseInt(inputVals[0], 10, 64)
+	incomingFloat, _ = strconv.ParseFloat(inputVals[1], 64)
+	incomingString = inputVals[2]
+
+	fmt.Println(i + uint64(incomingInt))
+	fmt.Printf("%.1f\n", d+incomingFloat)
+	fmt.Println(s + incomingString)
 }
